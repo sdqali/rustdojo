8780f1153f99db430b87fbe90b18d03e7a4818f8
Sadique Ali <sadiqalikm@gmail.com>
2016-06-09 15:12:00 -0400

    Display for complex struct.

--- src/display.rs
+++ src/display.rs
@@ -21,7 +21,23 @@ impl fmt::Display for Point2 {
     }
 }
 
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} + {}i", self.real, self.imag)
+    }
+}
+
 fn main() {
     let range =   MinMax(Point2 {x :13.4, y: 65.9}, Point2 {x :48.54, y: 176.98});
     println!("The range is {}", range);
+
+    let complex = Complex{real: 13.87, imag: 987.04};
+    println!("Complex number display: {}", complex);
+    println!("Complex number debug: {:?}", complex);
 }
